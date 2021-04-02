1713c941aa48a179e4314da9731e4a4379086275
Fatemeh-Soleimani <80667774+Fatemeh-Soleimani@users.noreply.github.com>
2021-04-02 20:00:36 +0430

    edit first ground

--- firstground.cpp
+++ firstground.cpp
@@ -7,9 +7,9 @@ class playground1
 public:
 	playground1();
 	void showboard();
-	bool gameover();
+	bool finish();
 	bool win();
-	bool push_back(char n, char m);
+	bool push_back(string n, string m);
 	
 private:
 	string name;
@@ -19,9 +19,8 @@ private:
 	bool columncrossed();
 	bool diagonalcrossed();
 };
-playground1::playground1()
-{
-	playground[0][0] = "1"
+playground1::playground1() {
+	playground[0][0] = "1";
 	playground[0][1] = "2";
 	playground[0][2] = "3";
 	playground[1][0] = "4";
@@ -31,24 +30,18 @@ playground1::playground1()
 	playground[2][1] = "8";
 	playground[2][2] = "9";
 }
-void playground1::showboard(char playground[3][3])
-{
-	printf("\n\n");
+void playground1::showboard() {
+	cout<<"\n\n";
 
-	printf("\t\t\t %c | %c | %c \n", playground[0][0],
-		playground[0][1], playground[0][2]);
-	printf("\t\t\t--------------\n");
-	printf("\t\t\t %c | %c | %c \n", playground[1][0],
-		playground[1][1], playground[1][2]);
-	printf("\t\t\t--------------\n");
-	printf("\t\t\t %c | %c | %c \n\n", playground[2][0],
-		playground[2][1], playground[2][2]);
-
+	cout << "\t\t\t" << playground[0][0] << " | " << playground[0][1] << " | " << playground[0][2] << "\n";
+	cout<<"\t\t\t--------------\n";
+	cout << "\t\t\t " << playground[1][0] << " | " << playground[1][1] << " | " << playground[1][2] << " \n";
+	cout<<"\t\t\t--------------\n";
+	cout << "\t\t\t " << playground[2][0] << " | " << playground[2][1] << " | " << playground[2][2] << " \n\n";
 	return;
 }
 
-bool playground1::gameover(char playground[][3])
-{
+bool playground1::finish() {
 	if (win() == true)
 		return true;
 	if (leftMoves() == 0)
@@ -57,22 +50,20 @@ bool playground1::gameover(char playground[][3])
 	return false;
 }
 bool playground1::win() {
-	return(rowcrossed(playground) || columncrossed(playground) || diagonalcrossed(playground));
+	return(rowcrossed() || columncrossed() || diagonalcrossed());
 }
 
-int playground1::leftMoves()
-{
+int playground1::leftMoves() {
 	int leftmoves = 9;
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
 		{
-			if (playground[i][j] == '*' || playground[i][j] == '#')
+			if (playground[i][j] == "*" || playground[i][j] == "#")
 				leftmoves--;
 		}
 	return leftmoves;
 }
-bool playground1::push_back(char n, char m)
-{
+bool playground1::push_back(string n, string m) {
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
 			if (playground[i][j] == n)
@@ -82,32 +73,29 @@ bool playground1::push_back(char n, char m)
 			}
 	return false;
 }
-bool playground1::rowcrossed(char playground[][3])
-{
+bool playground1::rowcrossed() {
 	for (int i = 0; i < 3; i++)
 	{
-		if (playground[i][0] == playground[i][1] && playground[i][1] == playground[i][2] && playground[i][0] != ' ')
+		if (playground[i][0] == playground[i][1] && playground[i][1] == playground[i][2] )
 			return (true);
 	}
 	return(false);
 }
 
-bool playground1:: columncrossed(char playground[][3])
-{
+bool playground1:: columncrossed() {
 	for (int i = 0; i < 3; i++)
 	{
-		if (playground[0][i] == playground[1][i] && playground[1][i] == playground[2][i] && playground[0][i] != ' ')
+		if (playground[0][i] == playground[1][i] && playground[1][i] == playground[2][i] )
 			return (true);
 	}
 	return(false);
 }
 
-bool playground1::diagonalcrossed(char playground[][3])
-{
-	if (playground[0][0] == playground[1][1] && playground[1][1] == playground[2][2] && playground[0][0] != ' ')
+bool playground1::diagonalcrossed() {
+	if (playground[0][0] == playground[1][1] && playground[1][1] == playground[2][2] )
 		return(true);
 
-	if (playground[0][2] == playground[1][1] && playground[1][1] == playground[2][0] && playground[0][2] != ' ')
+	if (playground[0][2] == playground[1][1] && playground[1][1] == playground[2][0] )
 		return(true);
 
 	return(false);
